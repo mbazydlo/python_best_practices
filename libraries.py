18a5f9f64c4e0bc168ae81b84acf012e227ba030
Marcin Bazydło <marcin.p.bazydlo@gmail.com>
2022-08-13 18:17:27 +0200

    Added libraries.py - no 2 practice - Use argparse instead of sys.argv

--- libraries.py
+++ libraries.py
@@ -16,7 +16,7 @@ We would like to generate new path in some other directory to copy that file. Ta
 
 """
 
-# os.path exaples
+# os.path examples
 import os
 
 some_path = '/home/user/app/some_dir/some_file.txt'
@@ -41,4 +41,80 @@ Warning: if you print result you see '/home/user/app/new_dir/some_file.txt', but
 __str__ implemented. If you want string representation use as_posix() method or just str() casting.
 
 Both libraries are built in.
+"""
+
+
+"""
+2. Use argparse instead of sys.argv
+
+sys.argv is simple function used to catch argument passed to file while running.
+
+For example:
+>>> python libraries.py arg_1 22 '{"foo": "bar"}'
+"""
+import sys
+print(sys.argv)  # result: ['libraries.py', 'Neverland', '22', '{foo: bar}']
+print(sys.argv[1])
+print(sys.argv[2])
+print(sys.argv[3])
+
+
+"""
+As we can see there are multiple issues with this solution:
+1. Always the first argument is file name, we have to always remember that.
+2. Arguments passed from terminal are always treated as strings and sys.arg doesn't have any casting mechanism built in.
+3. We are not able to pass arguments as keyword (e.g. --country Neverland) in simple way \
+    (without building some mechanism to parse it)
+4. As all arguments are position we can easily make mistake replacing some of indexes.
+
+Argparse provides us much better way to do it, solving all above issues. It may look a bit more difficult to code, \
+    but provides us straight and clear way to parsing arguments.
+"""
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--country')
+parser.add_argument('--number')
+parser.add_argument('--json')
+args = parser.parse_args()
+
+print(vars(args))  # {'country': 'Neverland', 'number': '22', 'json': '{"foo": "bar"}'}
+print(args.country)  # Neverland
+
+"""
+Above code was run with keyword arguments what make it much more clear.
+>>> python libraries.py --country Neverland --number 22 --json '{"foo": "bar"}'
+
+
+Argparse provides us multiple optional parameters to be set up, like type for automatic casting.
+Some of them use with our example:
+"""
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--country', required=True, help='Your favorite country')
+parser.add_argument('--number', type=int, required=True, help='Your favorite number')
+parser.add_argument('--json', type=json.loads, help='Additional informations')
+args = parser.parse_args()
+
+print(vars(args))  # {'country': 'Neverland', 'number': 22, 'json': {'foo': 'bar'}}
+
+"""
+If we have 'help' parameters provided then it's a great documentation for developers.
+In that case argparse also provide us information for file parameters:
+>>> python libraries.py -h
+
+///
+usage: libraries.py [-h] --country COUNTRY --number NUMBER [--json JSON]
+
+optional arguments:
+  -h, --help         show this help message and exit
+  --country COUNTRY  Your favorite country
+  --number NUMBER    Your favorite number
+  --json JSON        Additional informations
+///
+
+For further information check: https://docs.python.org/3/library/argparse.html
+Both libraries (argparse and sys) are built in.
 """
